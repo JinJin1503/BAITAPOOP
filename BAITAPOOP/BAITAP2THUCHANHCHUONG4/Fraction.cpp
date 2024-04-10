220c538d37613f5b9364a29ad35368efd3f66107
Ngô Gia Khánh <164596375+JinJin1503@users.noreply.github.com>
2024-04-10 21:13:54 +0700

    Add files via upload

---
+++ BAITAPOOP/BAITAP2THUCHANHCHUONG4/Fraction.cpp
@@ -0,0 +1,88 @@
+#include "Fraction.h"
+#include <cstdlib>
+#include <ctime>
+std::istream& operator>>(std::istream& in, Fraction& f) {
+    int num, den;
+    std::cout << "Nhap tu so: ";
+    in >> num;
+    f.setNumerator(num); // Use setNumerator to set the value of numerator
+    std::cout << "Nhap mau so: ";
+    in >> den;
+    f.setDenominator(den); // Use setDenominator to set the value of denominator
+    return in;
+}
+Fraction::Fraction(int num, int den) : numerator(num), denominator(den) {}
+
+Fraction::Fraction(int n) {
+    numerator = rand() % n + 1;
+    denominator = rand() % n + 1;
+}
+
+int Fraction::getNumerator() const {
+    return numerator;
+}
+
+int Fraction::getDenominator() const {
+    return denominator;
+}
+
+void Fraction::setNumerator(int num) {
+    numerator = num;
+}
+
+void Fraction::setDenominator(int den) {
+    denominator = den;
+}
+
+Fraction operator+(const Fraction& f1, const Fraction& f2) {
+    int num = f1.getNumerator() * f2.getDenominator() + f2.getNumerator() * f1.getDenominator();
+    int den = f1.getDenominator() * f2.getDenominator();
+    return Fraction(num, den);
+}
+
+Fraction operator-(const Fraction& f1, const Fraction& f2) {
+    int num = f1.getNumerator() * f2.getDenominator() - f2.getNumerator() * f1.getDenominator();
+    int den = f1.getDenominator() * f2.getDenominator();
+    return Fraction(num, den);
+}
+
+Fraction operator*(const Fraction& f1, const Fraction& f2) {
+    int num = f1.getNumerator() * f2.getNumerator();
+    int den = f1.getDenominator() * f2.getDenominator();
+    return Fraction(num, den);
+}
+
+Fraction operator/(const Fraction& f1, const Fraction& f2) {
+    int num = f1.getNumerator() * f2.getDenominator();
+    int den = f1.getDenominator() * f2.getNumerator();
+    return Fraction(num, den);
+}
+
+bool operator==(const Fraction& f1, const Fraction& f2) {
+    return (f1.getNumerator() == f2.getNumerator()) && (f1.getDenominator() == f2.getDenominator());
+}
+
+bool operator!=(const Fraction& f1, const Fraction& f2) {
+    return !(f1 == f2);
+}
+
+bool operator>=(const Fraction& f1, const Fraction& f2) {
+    return (f1.getNumerator() * f2.getDenominator() >= f2.getNumerator() * f1.getDenominator());
+}
+
+bool operator<=(const Fraction& f1, const Fraction& f2) {
+    return (f1.getNumerator() * f2.getDenominator() <= f2.getNumerator() * f1.getDenominator());
+}
+
+std::ostream& operator<<(std::ostream& out, const Fraction& f) {
+    out << f.getNumerator() << '/' << f.getDenominator();
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, Fraction& f) {
+    std::cout << "Nhap tu so: ";
+    in >> f.numerator;
+    std::cout << "Nhap mau so: ";
+    in >> f.denominator;
+    return in;
+}
---
+++ BAITAPOOP/BAITAP2THUCHANHCHUONG4/Fraction.h
@@ -0,0 +1,34 @@
+#pragma once
+#pragma once
+#ifndef FRACTION_H
+#define FRACTION_H
+
+#include <iostream>
+
+class Fraction {
+private:
+    int numerator;
+    int denominator;
+
+public:
+    Fraction(int num, int den);
+    Fraction(int n);
+
+    int getNumerator() const;
+    int getDenominator() const;
+    void setNumerator(int num);
+    void setDenominator(int den);
+};
+
+Fraction operator+(const Fraction& f1, const Fraction& f2);
+Fraction operator-(const Fraction& f1, const Fraction& f2);
+Fraction operator*(const Fraction& f1, const Fraction& f2);
+Fraction operator/(const Fraction& f1, const Fraction& f2);
+bool operator==(const Fraction& f1, const Fraction& f2);
+bool operator!=(const Fraction& f1, const Fraction& f2);
+bool operator>=(const Fraction& f1, const Fraction& f2);
+bool operator<=(const Fraction& f1, const Fraction& f2);
+std::ostream& operator<<(std::ostream& out, const Fraction& f);
+std::istream& operator>>(std::istream& in, Fraction& f);
+
+#endif 
